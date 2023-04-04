6b9a3daf38a6f440015f58a7fc786b4ab5af4e22
Maxime Lemoine <maxime.lemoine1@etu.univ-lehavre.fr>
2023-04-04 10:51:56 +0200

    ajout de méthodes dans CryptoMarche.java

--- src/CryptoMarche.java
+++ src/CryptoMarche.java
@@ -1,60 +1,79 @@
 import java.util.ArrayList;
 
-public class CryptoMarche {
+public class CryptoMarche
+{
 
-    private ArrayList<Portefeuille> portefeuilles;
-    private static CryptoMarche marche;
+	private ArrayList<Portefeuille> portefeuilles;
+	private static CryptoMarche marche;
 
-    private CryptoMarche(){
-        portefeuilles = new ArrayList<Portefeuille>();
-    }
+	private CryptoMarche()
+	{
+		portefeuilles = new ArrayList<Portefeuille>();
+	}
 
-    public static CryptoMarche getInstance(){
-        if(marche == null){ marche = new CryptoMarche();}
+	public static CryptoMarche getInstance()
+	{
+		if(marche == null){ marche = new CryptoMarche();}
 		return marche;
-    }
+	}
 
-    public void ajouter(Portefeuille p){
-        portefeuilles.add(p);
-    }
+	public void ajouter(Portefeuille p)
+	{
+		portefeuilles.add(p);
+	}
 
-    /**
-     * Cette fonction recherche sur le marché tous les portefeuilles 
-     * du propriétaire et calcule son capital en euros. 
-     * @param proprietare
-     * @return capital en euros du propriétare.
-     */
-    public double capitalEnEuros(String proprietaire){
-        /**
-			FONCTION À IMPLEMENTER
-        **/
+	/**
+	 * Cette fonction recherche sur le marché tous les portefeuilles 
+	 * du propriétaire et calcule son capital en euros. 
+	 * @param proprietare
+	 * @return capital en euros du propriétare.
+	 */
+	public double capitalEnEuros(String proprietaire)
+	{ //fonction implémentée
+		double capital = 0;
+		
+		for( Portefeuille p : portefeuilles )
+		{
+			if( p.estProprietaire(proprietaire) )
+			{
+				capital += p.getMontant();
+			}
+		}
 
-        return 0;
-    }
+		return capital;
+	}
 
-    /**
-     * Cette fonction recherche sur le marché tous les portefeuilles 
-     * d'un type de devise et calcule le volume total de capital de 
-     * cette devise sur le marché 
-     * @param monnaie
-     * @return capital total en circulation de la cryptomonnaie (en euros).
-     */
-    public double capitalMonneaie(Cryptomonnaie monnaie){
-        /**
-			FONCTION À IMPLEMENTER
-        **/
+	/**
+	 * Cette fonction recherche sur le marché tous les portefeuilles 
+	 * d'un type de devise et calcule le volume total de capital de 
+	 * cette devise sur le marché 
+	 * @param monnaie
+	 * @return capital total en circulation de la cryptomonnaie (en euros).
+	 */
+	public double capitalMonneaie(Cryptomonnaie monnaie)
+	{ //fonction implémentée
+		double capital=0;
+		
+		for( Portefeuille p : portefeuilles )
+		{
+			if( p.getMonnaie() == monnaie )
+			{
+				capital += p.getMontant();
+			}
+		}
 
-        return 0;
+		return capital;
+	}
 
-    }
+	@Override
+	public String toString()
+	{
+		String ret = "";
+		for(Portefeuille p : this.portefeuilles)
+		{
+			ret += p.toString() + "\n";
+		}
+		return ret;
+	}
 
-    @Override
-    public String toString() {
-        String ret = "";
-        for(Portefeuille p : this.portefeuilles){
-            ret += p.toString() + "\n";
-        }
-        return ret;
-    }
-
-}
+	}
